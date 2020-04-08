541382f31c5e582ceb1903fe8bf97e3abdff6c6c
Geoffrey Meier <geoffreymeier@u.boisestate.edu>
2020-04-07 20:32:34 -0600

    Implements homepage asthetics - task #24

--- index.php
+++ index.php
@@ -4,10 +4,18 @@
 
 <head>
     <link rel="stylesheet" href="styles.css">
+    <title>Planning Poker</title>
 </head>
 
 <body>
-    <a href="lobby.php">Lobby</a>
+    <header>
+        <h1>Planning Poker<hr></h1>
+    </header>
+
+    <main>
+        <p>Welcome to Planning Poker! This tool will help your scrum team vote on user stories and see detailed results, as well as make revoting on stories easier than ever. Click the button below to get started!</p>
+        <a href="lobby.php" id="getStarted">Let's get started!</a>
+    </main>
 </body>
 
 </html>
